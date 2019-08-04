2a772247a0676fd4dbfdc38e237a48233eb8334b
jmulford-bandwidth <jmulford@bandwidth.com>
2019-08-04 13:47:59 -0400

    Added basic env variable checking

--- ruby/BandwidthReferenceApp/messaging_app.rb
+++ ruby/BandwidthReferenceApp/messaging_app.rb
@@ -7,6 +7,16 @@
 
 require 'sinatra'
 
+MESSAGING_ACCOUNT_ID = (ENV.has_key?("MESSAGING_ACCOUNT_ID") ? ENV["MESSAGING_ACCOUNT_ID"]: nil)
+MESSAGING_API_TOKEN = (ENV.has_key?("MESSAGING_API_TOKEN") ? ENV["MESSAGING_API_TOKEN"]: nil)
+MESSAGING_API_SECRET = (ENV.has_key?("MESSAGING_API_SECRET") ? ENV["MESSAGING_API_SECRET"]: nil)
+MESSAGING_APPLICATION_ID = (ENV.has_key?("MESSAGING_APPLICATION_ID") ? ENV["MESSAGING_APPLICATION_ID"]: nil)
+
+if [MESSAGING_ACCOUNT_ID, MESSAGING_API_TOKEN, MESSAGING_API_SECRET, MESSAGING_APPLICATION_ID].include?(nil)
+    puts "Please set the MESSAGING environmental variables defined in the README"
+    exit(-1)
+end
+
 get '/message' do
-  return "Message"
+    return "Message"
 end
--- ruby/BandwidthReferenceApp/voice_app.rb
+++ ruby/BandwidthReferenceApp/voice_app.rb
@@ -7,6 +7,16 @@
 
 require 'sinatra'
 
+VOICE_ACCOUNT_ID = (ENV.has_key?("VOICE_ACCOUNT_ID") ? ENV["VOICE_ACCOUNT_ID"]: nil)
+VOICE_API_USERNAME = (ENV.has_key?("VOICE_API_USERNAME") ? ENV["VOICE_API_USERNAME"]: nil)
+VOICE_API_PASSWORD = (ENV.has_key?("VOICE_API_PASSWORD") ? ENV["VOICE_API_PASSWORD"]: nil)
+VOICE_APPLICATION_ID = (ENV.has_key?("VOICE_APPLICATION_ID") ? ENV["VOICE_APPLICATION_ID"]: nil)
+
+if [VOICE_ACCOUNT_ID, VOICE_API_USERNAME, VOICE_API_PASSWORD, VOICE_APPLICATION_ID].include?(nil)
+    puts "Please set the VOICE environmental variables defined in the README"
+    exit(-1)
+end
+
 get '/voice' do
   return "Voice"
 end
