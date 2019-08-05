81a59067959c5aa51b2042a4c275870b9df0beb0
John Chavez <52748091+jchavez443@users.noreply.github.com>
2019-08-05 08:56:09 -0400

    Dx-579  (#5)
    * DX-579 Added the Java demo app
    
    * Minor IDE changes
    
    * IDE changes
    
    * Added comments to the example app
    
    * Updated the JAVA example app based on PR review
    
    * removed commented out code
    
    * Removed phonenumber
    
    * Added comment on gatherURL about realtive paths

---
+++ java/JavaDemo/src/main/java/com/bandwidth/controller/MessageController.java
@@ -0,0 +1,166 @@
+package com.bandwidth.controller;
+
+import com.bandwidth.messaging.APIHelper;
+import com.bandwidth.messaging.BandwidthV2MessagingClient;
+import com.bandwidth.messaging.Configuration;
+import com.bandwidth.messaging.Environments;
+import com.bandwidth.messaging.controllers.APIController;
+import com.bandwidth.messaging.exceptions.APIException;
+import com.bandwidth.messaging.models.BandwidthCallbackMessage;
+import com.bandwidth.messaging.models.Media;
+import com.bandwidth.messaging.models.MessageRequest;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+import static spark.Spark.post;
+
+import static com.bandwidth.enviroment.Properties.getProperty;
+
+/**
+ * Controller to handle the Bandwidth message API
+ */
+public class MessageController {
+
+
+    private static Configuration msgConfig = new Configuration().newBuilder()
+            .basicAuthPassword( getProperty("message.api.secret") )
+            .basicAuthUserName( getProperty("message.api.token") )
+            .environment(Environments.PRODUCTION)
+            .build();
+
+    private static APIController msgClient =  new BandwidthV2MessagingClient(msgConfig).getClient();
+
+    private  static String msgUserId =  getProperty("message.account.id") ;
+
+    private static String applicationId = getProperty("message.application.id");
+
+
+    /**
+     * Uploads a media file from the disk to the Bandwidth network
+     * @param fileURL
+     * @param contentType
+     * @param mediaId
+     */
+    public static void uploadMedia(String fileURL, String contentType, String mediaId) {
+
+       File file = new File(fileURL);
+
+        try {
+            msgClient.uploadMedia(msgUserId, mediaId, file.length(), Files.readAllBytes(file.toPath()) ,contentType, "no-cache" );
+        } catch (APIException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Downloads media from the Bandwidth network to local
+     */
+    public static void downloadMedia(){
+
+    }
+
+    /**
+     * List the media in the user's account
+     * @return
+     */
+    public static List<Media> listMedia() {
+
+        List<Media> list = null;
+        try {
+            list = msgClient.listMedia(msgUserId,"");
+        } catch (APIException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+       return list;
+
+    }
+
+    /**
+     * Starts a post http listner for an incoming message.
+     * <br/>
+     * If the incoming message text is "call me" it will initate a voice call with the text sender
+     * <br/>
+     * If the incoming message contains media it will send the media back to the sender
+     * <br/>
+     * If the incoming message is not "call me" and contains no media it will reply with a sentence to the sender.
+     */
+    public static void listenReplyToMessage() {
+
+        post("/msg/incoming", (request, response) -> {
+
+            String json = request.body();
+
+            BandwidthCallbackMessage[] callbackMessages = APIHelper.deserialize(json, BandwidthCallbackMessage[].class);
+
+            if(callbackMessages == null || callbackMessages.length == 0  ){
+                //Incorrect format return
+                return "";
+            }
+
+            if(callbackMessages[0].getMessage() != null ){
+                System.out.println(callbackMessages[0].getType() +
+                        ", From:  " + callbackMessages[0].getMessage().getFrom() +
+                        ", To:  " +callbackMessages[0].getMessage().getTo() +
+                        ", Text:  " + callbackMessages[0].getMessage().getText());
+            }
+
+
+            if("message-delivered".equalsIgnoreCase(callbackMessages[0].getType()) || "message-failed".equalsIgnoreCase(callbackMessages[0].getType())){
+                //Message delivery notice or message failed notice.  Return 200 to Bandwidth.
+                return "";
+            }
+
+            // Incoming message to application # callbackMessages[0].getType() equals "message-received"
+
+            //For inbound messages, the From number is the number that sent the text
+            String from = callbackMessages[0].getMessage().getFrom();
+
+            //The inbound message From number needs to be set as the "To" number
+            //in order to reply back to the message
+            List<String> sendToNums = new ArrayList<>();
+            sendToNums.add(from);
+
+            //The inbound message "To" list is the numbers the sender messaged
+            String to = callbackMessages[0].getMessage().getTo().get(0);
+
+            MessageRequest msgRequest = new MessageRequest();
+            msgRequest.setApplicationId(applicationId);
+            msgRequest.setFrom(to);//the number the inbound message was sent to is now the from.
+            msgRequest.setTo(sendToNums);
+
+            String incomingText = callbackMessages[0].getMessage().getText();
+
+            List<String> incomingMedia = callbackMessages[0].getMessage().getMedia();
+
+            if("call me".equalsIgnoreCase(incomingText.trim())){
+                VoiceController.makeOutboundCall(from, to);
+                return "";
+            } else if( incomingMedia == null || incomingMedia.isEmpty() ) {
+                msgRequest.setText("The quick brown fox jumps over a lazy dog.");
+            } else {
+
+                //Download the incoming media to temp area on disk
+
+                //Upload the media from the disk to bandwidth with diffrent name
+
+                //Send the new media back to the texter
+
+                msgRequest.setMedia(incomingMedia);
+            }
+
+            msgClient.createMessage(msgUserId, msgRequest);
+
+            return "";
+        });
+    }
+
+
+}
