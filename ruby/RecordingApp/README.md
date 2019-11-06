cb4b09763e7de9bbb75d80b904c4db0887825eb2
jmulford-bandwidth <jmulford@bandwidth.com>
2019-11-06 13:12:11 -0500

    Added basic recording sample app

---
+++ ruby/RecordingApp/README.md
@@ -0,0 +1,42 @@
+<div align="center">
+
+# Voicemail Service
+
+<a href="http://dev.bandwidth.com"><img src="https://s3.amazonaws.com/bwdemos/BW_Voice.png"/></a>
+</div>
+
+## Description
+A sample app that mimics a voicemail service using Bandwidth's Voice API and Recording
+
+## How It Works
+When someone calls the number, they are given a prompt to leave a voicemail. After a timeout of 3 minutes or `#` is pressed, the recording ends. The user has the option of hearing the recording, and re-recording if necessary.
+
+Once the recording is done, the user will receive an MMS with the recording.
+
+## Running The App
+
+### Environmental Variables
+The following environmental variables need to be set
+
+| Variable | Description |
+|--|--|
+| MESSAGING_ACCOUNT_ID | Your Bandwidth Messaging account ID |
+| MESSAGING_API_TOKEN | Your Bandwidth Messaging API token |
+| MESSAGING_API_SECRET | Your Bandwidth Messaging API secret |
+| MESSAGING_APPLICATION_ID | Your Bandwidth Messaging application ID |
+| VOICE_ACCOUNT_ID | Your Bandwidth Voice account ID |
+| VOICE_API_USERNAME | Your Bandwidth Voice API username |
+| VOICE_API_PASSWORD | Your Bandwidth Voice API password |
+| VOICE_APPLICATION_ID | Your Bandwidth Voice application ID |
+| BASE_URL | The base url of the server running this application |
+
+### Commands
+Run the following commands to get started
+
+```
+gem install bundler
+bundle install
+ruby app.rb -p <port>
+```
+
+where `port` is the port to run the app on. Unassigned, `port` will default to `4567`.
---
+++ ruby/RecordingApp/app.rb
@@ -0,0 +1,170 @@
+# app.rb
+#
+# Sinatra app to run the recording sample app
+#
+# @copyright Bandwidth INC
+
+require 'sinatra'
+require 'bandwidth'
+
+include Bandwidth
+include Bandwidth::Voice
+include Bandwidth::Messaging
+
+begin
+    MESSAGING_ACCOUNT_ID = ENV.fetch("MESSAGING_ACCOUNT_ID")
+    MESSAGING_API_TOKEN = ENV.fetch("MESSAGING_API_TOKEN")
+    MESSAGING_API_SECRET = ENV.fetch("MESSAGING_API_SECRET")
+    MESSAGING_APPLICATION_ID = ENV.fetch("MESSAGING_APPLICATION_ID")
+    VOICE_ACCOUNT_ID = ENV.fetch("VOICE_ACCOUNT_ID")
+    VOICE_API_USERNAME = ENV.fetch("VOICE_API_USERNAME")
+    VOICE_API_PASSWORD = ENV.fetch("VOICE_API_PASSWORD")
+    VOICE_APPLICATION_ID = ENV.fetch("VOICE_APPLICATION_ID")
+    BASE_URL = ENV.fetch("BASE_URL")
+rescue
+    puts "Please set the environmental variables defined in the README"
+    exit(-1)
+end
+
+bandwidth_client = Bandwidth::Client.new(
+    voice_basic_auth_user_name: VOICE_API_USERNAME,
+    voice_basic_auth_password: VOICE_API_PASSWORD,
+    messaging_basic_auth_user_name: MESSAGING_API_TOKEN,
+    messaging_basic_auth_password: MESSAGING_API_SECRET,
+)
+
+$voice_client = bandwidth_client.voice_client.client
+$messaging_client = bandwidth_client.messaging_client.client
+
+post "/VoiecCallback" do
+    ring_audio = Bandwidth::Voice::PlayAudio.new({
+        :url => "some url"
+    })
+    leave_voicemail = Bandwidth::Voice::SpeakSentence.new({
+        :sentence => "Please leave a message after the beep. Your time limit is 3 minutes. Press # to stop the recording early"
+    })
+    beep_audio = Bandwidth::Voice::PlayAudio.new({
+        :url => "some url"
+    })
+    redirect = Bandwidth::Voice::Redirect.new({
+        :redirect_url => "/RecordCallback"
+    }) 
+    response = Bandwidth::Voice::Response.new()
+    response.push(ring_audio)
+    response.push(leave_voicemail)
+    response.push(beep_audio)
+    response.push(redirect)
+    return response.to_bxml()
+end
+
+post "/RecordCallback" do
+    start_recording = Bandwidth::Voice::Record.new({
+        :record_complete_url => "/RecordCompleteCallback",
+        :record_complete_method => "POST",
+        :recording_available_url => "/RecordingAvailableCallback",
+        :recording_available_method => "POST",
+        :max_duration => 180,
+        :terminating_digits => "#"
+    })
+    response = Bandwidth::Voice::Response.new()
+    response.push(start_recording)
+    return response.to_bxml()
+end
+
+post "/RecordCompleteCallback" do
+    #Loops endlessly until the recording is available
+    pause = Bandwidth::Voice::Pause.new({
+        :duration => 3
+    })
+    redirect = Bandwidth::Voice::Redirect.new({
+        :redirect_url => "/RecordCompleteCallback"
+    })
+    response = Bandwidth::Voice::Response.new()
+    response.push(pause)
+    response.push(redirect)
+    return response.to_bxml()
+end
+
+post "/RecordingAvailableCallback" do
+    data = JSON.parse(request.body.read)
+    if data["status"] == "complete" 
+        #Update call to get bxml at "/AskToHearRecordingGather" with the recording id as the tag
+        body = ApiModifyCallRequest.new
+        body.redirect_url = BASE_URL + "/AskToHearRecordingGather" #TODO: Can this be a relative url?
+        body.tag = data["recordingId"]
+        
+        begin
+            $voice_client.modify_call(VOICE_ACCOUNT_ID, data["callId"], body: body)
+        rescue Exception => e
+            puts e
+        end
+    end
+end
+
+post "/AskToHearRecordingGather" do
+    #ID of recording is in the tag
+    #TBD: Do we need the call id too?
+    data = JSON.parse(request.body.read)
+    ask_to_hear_recording = Bandwidth::Voice::SpeakSentence.new({
+        :sentence = "Your recording is now available. If you'd like to hear your recording, press 1, otherwise please hangup"
+    })
+    gather = Bandwidth::Voice::Gather.new({
+        :timeout => 15,
+        :speak_sentence = ask_to_hear_recording,
+        :max_digits => 1,
+        :gather_url => "/AskToHearRecordingEndGather",
+        :tag => data["tag"]
+    })
+    response = Bandwidth::Voice::Response.new()
+    response.push(gather)
+    return response.to_bxml()
+end
+
+post "/AskToHearRecordingEndGather" do
+    #ID of recording is in the tag
+    #TBD: Do we need the call id too?
+    data = JSON.parse(request.body.read)
+    response = Bandwidth::Voice::Response.new()
+    if data.key?("digits") and data["digits"] == "1"
+        #play recording
+        play_recording = Bandwidth::Voice::PlayAudio.new({
+            :url => "url of recording"
+        })
+        ask_to_re_record = Bandwidth::Voice::SpeakSentence.new({
+            :sentence = "Would you like to re record? Press 1 if so, otherwise please hangup"
+        })
+        
+        gather = Bandwidth::Voice::Gather.new({
+            :timeout => 15,
+            :speak_sentence = ask_to_re_record,
+            :max_digits => 1,
+            :gather_url => "/AskToReRecordEndGather",
+        })
+        response.push(play_recording)
+        response.push(gather)
+    else
+        hangup = Bandwidth::Voice::Hangup.new()
+        response.push(hangup)
+    end
+    return response.to_bxml()
+end
+
+post "/AskToReRecordEndGather" do
+    data = JSON.parse(request.body.read)
+    response = Bandwidth::Voice::Response.new()
+    if data.key?("digits") and data["digits"] == "1"
+        beep_audio = Bandwidth::Voice::PlayAudio.new({
+            :url => "some url"
+        })
+        redirect = Bandwidth::Voice::Redirect.new({
+            :redirect_url => "/RecordCallback"
+        })
+        response.push(beep_audio)
+        response.push(redirect)
+    else
+        hangup = Bandwidth::Voice::Hangup.new()
+        response.push(hangup)
+    end
+    return response.to_bxml()
+end
+
