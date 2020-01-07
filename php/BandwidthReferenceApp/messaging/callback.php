36cf349877b0f75c1930bafa83efa0cacd61efb4
jmulford-bandwidth <jmulford@bandwidth.com>
2020-01-07 14:15:31 -0500

    Added non functional code that should work

--- php/BandwidthReferenceApp/messaging/callback.php
+++ php/BandwidthReferenceApp/messaging/callback.php
@@ -30,6 +30,9 @@ $client = new BandwidthLib\BandwidthClient($config);
 
 $messagingClient = $client->getMessaging()->getClient();
 
+//needed to attach media to outbound MMS after uploading it to Bandwidth
+$mediaEndpoint = "https://messaging.bandwidth.com/api/v2/users/" . $MESSAGING_ACCOUNT_ID . "/media/";
+
 $data = json_decode(file_get_contents('php://input'), true);
 
 /*
@@ -45,10 +48,52 @@ if ($data[0]["type"] == "message-received") {
         echo "NYI";
     }
     elseif (array_key_exists("media", $data[0]["message"])) {
-        echo "NYI";
+        //download media from the text message//
+        //grab the media ID based on the "media" field
+        $mediaIds = [];
+        foreach ($data[0]["message"]["media"] as $mediaUrl) {
+            $mediaUrlPieces = explode("/", $mediaUrl);
+            $mediaId = NULL;
+            //handle media URLs of the format https://messaging.bandwidth.com/api/v2/users/123/media/file.png
+            if (($mediaUrlPieces[count($mediaUrlPieces)-2]) == "media") {
+                $mediaId = end($mediaUrlPieces);
+            }
+            //handle media urls of the format https://messaging.bandwidth.com/api/v2/users/123/media/abc/0/file.png
+            else {
+                $mediaId = implode("%2F", array_splice($mediaUrlPieces, -3, count($mediaUrlPieces), true));
+            }
+            array_push($mediaIds, $mediaId);
+        }
+        //download media into memory via API request
+        
+        $downloadedMedia = [];
+        foreach ($mediaIds as $mediaId) {
+            array_push($downloadedMedia, $messagingClient->getMedia($MESSAGING_ACCOUNT_ID, $mediaId)->getResult());
+        }
+        //reupload media using an API request//
+        for ($i = 0; $i < count($mediaIds); $i+=1) {
+            $media = $downloadedMedia[$i];
+            $mediaId = $mediaIds[$i];
+            $messagingClient->uploadMedia($MESSAGING_ACCOUNT_ID, $mediaId, strlen($media), $media);
+        }
+        //send message//
+        $to = array($data[0]["message"]["from"]);
+        $from = $data[0]["message"]["to"][0];
+        $fullMediaIds = [];
+        foreach ($mediaIds as $mediaId) {
+            array_push($fullMediaIds, $mediaEndpoint . $mediaId);
+        }
+
+        $body = new BandwidthLib\Messaging\Models\MessageRequest();
+        $body->applicationId = $MESSAGING_APPLICATION_ID;
+        $body->to = $to;
+        $body->from = $from;
+        $body->media = $fullMediaIds;
+        $body->text = "Rebound!";
+
+        $messagingClient->createMessage($MESSAGING_ACCOUNT_ID, $body);
     }
     else {
-        //handleInboundSms(array($data[0]["message"]["from"]), $data[0]["message"]["to"][0]);
         $to = array($data[0]["message"]["from"]);
         $from = $data[0]["message"]["to"][0];
 
