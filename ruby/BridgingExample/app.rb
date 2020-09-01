a5e0eced29e688e854f10120e3ab358d991151db
AJ Rice <53190766+ajrice6713@users.noreply.github.com>
2020-09-01 14:51:04 -0400

    Update app.rb

--- ruby/BridgingExample/app.rb
+++ ruby/BridgingExample/app.rb
@@ -7,6 +7,8 @@
 
 require 'sinatra'
 require 'bandwidth'
+require 'uri'
+
 
 include Bandwidth
 include Bandwidth::Voice
@@ -16,6 +18,8 @@ begin
   BANDWIDTH_API_USER                 = ENV.fetch('BANDWIDTH_API_USER')
   BANDWIDTH_API_PASSWORD             = ENV.fetch('BANDWIDTH_API_PASSWORD')
   BANDWIDTH_VOICE_APPLICATION_ID     = ENV.fetch('BANDWIDTH_VOICE_APPLICATION_ID')
+  PERSONAL_NUMBER                    = ENV.fetch('PERSONAL_NUMBER')
+  BASE_URL                           = ENV.fetch('BASE_URL') # in 123456abcd.ngrok.io format - no 'http://' or trailing '/'
 rescue
   puts 'Please set the environmental variables defined in the README'
   exit(-1)
@@ -26,49 +30,150 @@ bandwidth_client = Bandwidth::Client.new(
   voice_basic_auth_password: BANDWIDTH_API_PASSWORD
 )
 
+$voice_client = bandwidth_client.voice_client.client
 
-# Create functions for creating outbound call and updating a-Leg
 def createOutboundCall(to, from, callId)
-
+  body = ApiCreateCallRequest.new
+  body.from = from
+  body.to = PERSONAL_NUMBER
+  body.answer_url = URI::HTTP.build(host: BASE_URL, path: '/Outbound/Answer')
+  body.application_id = BANDWIDTH_VOICE_APPLICATION_ID
+  body.tag = callId
+  body.disconnect_url = URI::HTTP.build(host: BASE_URL, path: '/Disconnect')
+  body.disconnect_method = 'POST'
+  begin
+      result = $voice_client.create_call(BANDWIDTH_ACCOUNT_ID, :body => body)
+  rescue Exception => e
+      puts e
+  end
 end
 
 
 def updateCall(callId)
-
+  body = ApiModifyCallRequest.new
+  body.redirect_url = URI::HTTP.build(host: BASE_URL, path: '/UpdateCall')
+  body.state = "active"
+  begin
+      $voice_client.modify_call(BANDWIDTH_ACCOUNT_ID, callId, :body => body)
+  rescue Exception => e
+      puts e
+  end
 end
 
 
 # Define routes
 post '/Inbound/VoiceCallback' do
+  data = JSON.parse(request.body.read)
+  createOutboundCall(data['to'], data['from'], data['callId'])
 
+  response = Bandwidth::Voice::Response.new()
+  speak_sentence = Bandwidth::Voice::SpeakSentence.new({
+      :sentence => "Connecting your call, please wait.",
+      :voice => "julie"
+  })
+  ring = Bandwidth::Voice::Ring.new({
+    :duration => '30'
+  })
+  redirect = Bandwidth::Voice::Redirect.new({
+    :redirect_url => URI::HTTP.build(host: BASE_URL, path: '/UpdateCall')
+  })
+  response.push(speak_sentence)
+  response.push(ring)
+  response.push(redirect)
+  status 200
+  return response.to_bxml()
 end
 
 
 post '/Outbound/Answer' do
-
+  data = JSON.parse(request.body.read)
+  if data['eventType'] != 'answer'
+    return updateCall(data['tag'])
+  else
+    response = Bandwidth::Voice::Response.new()
+    speak_sentence = Bandwidth::Voice::SpeakSentence.new({
+        :sentence => "Please press 1 to accept the call, or any other button to send to voicemail",
+        :voice => "julie"
+    })
+    gather = Bandwidth::Voice::Gather.new({
+        :gather_url => URI::HTTP.build(host: BASE_URL, path: '/Outbound/Gather'),
+        :terminating_digits => "#",
+        :max_digits => '1',
+        :first_digit_timeout => "10",
+        :speak_sentence => speak_sentence,
+        :tag => data['tag']
+    })
+    response.push(gather)
+    status 200
+    return response.to_bxml()
+  end
 end
 
 
 post '/Outbound/Gather' do
-
+  data = JSON.parse(request.body.read)
+  if data['digits'] == '1'
+    response = Bandwidth::Voice::Response.new()
+    speak_sentence = Bandwidth::Voice::SpeakSentence.new({
+        :sentence => "The bridge will start now",
+        :voice => "julie"
+    })
+    bridge = Bandwidth::Voice::Bridge.new({
+        :call_id => data['tag']
+    })
+    response.push(speak_sentence)
+    response.push(bridge)
+    status 200
+    return response.to_bxml()
+  else
+    updateCall(data['tag'])
+    status 204
+  end
 end
 
 
 post '/Disconnect' do
-
+  data = JSON.parse(request.body.read)
+  if data['eventType'] != 'timeout'
+    updateCall(data['tag'])
+    status 204
+  end
 end
 
 
 post '/UpdateCall' do
-
+  data = JSON.parse(request.body.read)
+  response = Bandwidth::Voice::Response.new()
+  speak_sentence = Bandwidth::Voice::SpeakSentence.new({
+      :sentence => "The person you are trying to reach is not available, please leave a message at the tone",
+      :voice => "julie"
+  })
+  play_audio = Bandwidth::Voice::PlayAudio.new({
+    :url => "https://www.soundjay.com/button/sounds/beep-01a.wav"
+  })
+  record = Bandwidth::Voice::Record.new({
+      :recording_available_url => URI::HTTP.build(host: BASE_URL, path: '/Recording'),
+      :max_duration => "30"
+  })
+  response.push(speak_sentence)
+  response.push(play_audio)
+  response.push(record)
+  status 200
+  return response.to_bxml()
 end
 
 
 post '/Recording' do
-
+  data = JSON.parse(request.body.read)
+  File.open("./Recordings/" + data['recordingId'].to_s + '.wav', "wb") do |f|
+    response = $voice_client.get_stream_recording_media(BANDWIDTH_ACCOUNT_ID, data['callId'], data['recordingId'])
+    f.puts(response.data)
+  end
+  status 204
 end
 
 
 post '/Status' do
 
+  status 204
 end
