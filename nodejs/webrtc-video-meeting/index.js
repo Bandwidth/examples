53690de48994cdcb0fed26140d7bd82d3b17315c
Adam Covati <acovati@bandwidth.com>
2020-12-10 14:31:20 -0500

    Sample Nodejs Video API app (#71)
    * Initial commit of a relatively simple meeting app that support multiple rooms

---
+++ nodejs/webrtc-video-meeting/index.js
@@ -0,0 +1,163 @@
+const BandwidthWebRTC = require("@bandwidth/webrtc");
+const express = require("express");
+const uuid = require("uuid");
+const dotenv = require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// global vars
+const port = 3000;
+const accountId = process.env.ACCOUNT_ID;
+var rooms_db = new Map();
+
+BandwidthWebRTC.Configuration.basicAuthUserName = process.env.USERNAME;
+BandwidthWebRTC.Configuration.basicAuthPassword = process.env.PASSWORD;
+var webRTCController = BandwidthWebRTC.APIController;
+
+app.post("/joinCall", async (req, res) => {
+  console.log(`joinCall> about to setup browser client, data: '${req.body}'`);
+  console.log(req.body);
+
+  // setup the session and add this user into it
+  var room;
+  try {
+    console.log(`Adding ${req.body.caller.name} to session`);
+    var [participant, token] = await createParticipant(
+      accountId,
+      req.body.audio,
+      req.body.video,
+      uuid.v1()
+    );
+
+    room = await addParticipantToRoom(accountId, participant.id, req.body.room);
+  } catch (error) {
+    console.log("Failed to start the browser call:", error);
+    return res.status(500).send({ message: "failed to set up participant" });
+  }
+
+  // now that we have added them to the session,
+  //  we can send back the token they need to join
+  //  as well as info about the room they are in
+  res.send({
+    message: "created particpant and setup session",
+    token: token,
+    room: room,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+//
+// Calling out to Bandwidth functions
+//
+/**
+ *  Create a new participant
+ * @param account_id
+ * @param audio_perm boolean for audio permission
+ * @param video_perm boolean for video permission
+ * @param tag to tag the participant with, no PII should be placed here
+ * @return list: (a Participant json object, the participant token)
+ */
+async function createParticipant(account_id, audio_perm, video_perm, tag) {
+  perms = [];
+  if (audio_perm) perms.push("AUDIO");
+  if (video_perm) perms.push("VIDEO");
+  // create a participant for this browser user
+  var participantBody = new BandwidthWebRTC.Participant({
+    tag: tag,
+    publishPermissions: perms,
+  });
+
+  try {
+    let createResponse = await webRTCController.createParticipant(
+      accountId,
+      participantBody
+    );
+
+    return [createResponse.participant, createResponse.token];
+  } catch (error) {
+    console.log("failed to create Participant", error);
+    throw new Error(
+      "Failed to createParticipant, error from BAND:" + error.errorMessage
+    );
+  }
+}
+
+/**
+ * @param account_id The id for this account
+ * @param participant_id a Participant id
+ * @param room_name The room to add this participant to
+ * @return room in case you want any details about the state of the room
+ */
+async function addParticipantToRoom(account_id, participant_id, room_name) {
+  room = await getRoom(accountId, room_name);
+
+  var body = new BandwidthWebRTC.Subscriptions({ sessionId: room.session_id });
+
+  try {
+    await webRTCController.addParticipantToSession(
+      accountId,
+      room.session_id,
+      participant_id,
+      body
+    );
+  } catch (error) {
+    console.log("Error on addParticipant to Session:", error);
+    throw new Error(
+      "Failed to addParticipantToSession, error from BAND:" + error.errorMessage
+    );
+  }
+
+  // update the room with the new participant
+  room.participants.push(participant_id);
+  rooms_db.set(room_name, room);
+
+  return room;
+}
+
+/**
+ * Create a room or return it if it's an existing one
+ * @param account_id
+ * @param room_name the room you are joining
+ * @return the room for this session
+ */
+async function getRoom(account_id, room_name) {
+  // check if we've already created a session for this call
+  //  - this is a simplification we're doing for this demo
+  if (rooms_db.has(room_name)) {
+    return rooms_db.get(room_name);
+  }
+
+  console.log(`No room/session found, creating '${room_name}'`);
+  // otherwise, create the session
+  // tags are useful to audit or manage billing records
+  let sessionBody = new BandwidthWebRTC.Session({ tag: `demo.${room_name}` });
+  let sessionResponse;
+  try {
+    sessionResponse = await webRTCController.createSession(
+      account_id,
+      sessionBody
+    );
+  } catch (error) {
+    console.log("getRoom> Failed to create room/session:", error);
+    throw new Error(
+      "Error in createSession, error from BAND:" + error.errorMessage
+    );
+  }
+
+  // saves it for future use, this would normally be stored with meeting/call/appt details
+  room = {
+    name: room_name,
+    session_id: sessionResponse.id,
+    participants: [],
+    calls: [],
+  };
+  rooms_db.set(room_name, room);
+
+  return room;
+}
---
+++ nodejs/webrtc-video-meeting/public/webrtc_mgr.js
@@ -0,0 +1,521 @@
+const bandwidthRtc = new BandwidthRtc();
+//
+// configuration
+const DEBUG = true;
+const statusDiv = null;
+// a div to append log messages to on screen
+const logDiv = false;
+// a div to append new media elements to (video or audio)
+//  not needed if you implement onNewStream
+const mediaDiv = false;
+const videoEnabled = true;
+// ring while waiting for first connection
+const enableRinging = false;
+// when media connects, mute your mic by default
+const start_muted_audio = false;
+
+//
+//  internal global vars, don't set these
+// universal id for the call (the server tells us this)
+let internal_call_id = "";
+let other_callers = [];
+
+// global vars
+var my_media_stream;
+var my_screen_stream;
+var local_video_stream = false;
+
+/**
+ * Get the token required to auth with the media server
+ * Use that token to start streaming
+ * @param call_info json object with the following
+ *  caller: {name: "adam"}
+ *  call_type: phone", or "push"
+ *  room: room_name
+ *  audio: true OR false (usually true)
+ *  video: true OR false
+ */
+async function getOnline(call_info) {
+  // prevent double clicks
+  console.log("Fetching token from server for: ");
+  console.log(call_info);
+
+  updateStatus("Call Setup");
+  try {
+    // call your server function that does call control
+    var res = await fetch("/joinCall", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(call_info),
+    });
+  } catch (error) {
+    console.log(`getOnline> error on fetch ${error}`);
+    return;
+  }
+  // basic error handling
+  if (res.status !== 200) {
+    alert("getOnline> got back non-200: " + res.status);
+    console.log(res);
+  } else {
+    const json = await res.json();
+    console.log(json);
+
+    // save this for later use to sign off gracefully
+    internal_call_id = json.room.name;
+
+    // ring if desired, ring until someone joins
+    if (enableRinging) {
+      playAudio("ring", "/ring.mp3");
+    }
+
+    startStreaming(json.token, call_info);
+  }
+}
+
+/**
+ * Now that we have the token, we can start streaming media
+ * The token param is fetched from the server above
+ */
+async function startStreaming(token, call_info) {
+  // Connect to Bandwidth WebRTC
+  await bandwidthRtc.connect({ deviceToken: token });
+  console.log("connected to bandwidth webrtc!");
+  //
+  // Publish the browser's microphone and video if appropriate
+  // set the video constraints
+  var video_constraints = false;
+  if (call_info.video) {
+    video_constraints = {
+      frameRate: 30,
+      width: { min: 320, max: 640 },
+      height: { min: 240, max: 480 },
+      resizeMode: "crop-and-scale",
+    };
+
+    if (call_info.video_device == "none") {
+      video_constraints = false;
+    } else if (call_info.video_device != "default") {
+      video_constraints.deviceId = { exact: call_info.video_device };
+    } // if it is default, we'll let getUserMedia decide
+  }
+
+  // set the audio constraints
+  var audio_constraints = false;
+  if (call_info.audio) {
+    audio_constraints = {
+      echoCancellation: true,
+    };
+    if (call_info.mic_device == "none") {
+      audio_constraints = false;
+    } else if (call_info.mic_device != "default") {
+      audio_constraints.deviceId = { exact: call_info.mic_device };
+    } // if it is default, we'll let getUserMedia decide
+  }
+
+  streamResp = await bandwidthRtc.publish({
+    audio: audio_constraints,
+    video: video_constraints,
+  });
+  my_media_stream = streamResp.mediaStream;
+  if (start_muted_audio) {
+    mute();
+  }
+  updateStatus("Online...");
+  console.log(
+    `browser mic is streaming with stream id: ${streamResp.mediaStream.id}`
+  );
+}
+
+/**
+ * Start or stop screensharing in an already online/publishing state
+ */
+async function screenShare() {
+  // if we're already sharing, then stop
+  if (my_screen_stream) {
+    // unpublish
+    await bandwidthRtc.unpublish(my_screen_stream.endpointId);
+
+    // stop the tracks locally
+    var tracks = my_screen_stream.getTracks();
+    tracks.forEach(function (track) {
+      console.log(`stopping stream`);
+      console.log(track);
+      track.stop();
+    });
+
+    my_screen_stream = null;
+  } else {
+    // we're not sharing, so start
+    video_constraints = {
+      frameRate: 30,
+    };
+    // getDisplayMedia is the magic function for screen/window/tab sharing
+    my_screen_stream = await navigator.mediaDevices.getDisplayMedia({
+      audio: false,
+      video: video_constraints,
+    });
+
+    // start the share and save the endPointId so we can unpublish later
+    var resp = await bandwidthRtc.publish(my_screen_stream);
+    my_screen_stream.endpointId = resp.endpointId;
+  }
+}
+
+/**
+ * Just get this user offline, don't end the session for everyone
+ */
+async function signOff() {
+  bandwidthRtc.disconnect();
+  // remove the others from this screen
+  other_callers.forEach(function (caller) {
+    disconnectEndpoint(caller);
+  });
+}
+
+/**
+ * End the current session for all callers
+ * uses internal_call_id to know what to end, set when you got online
+ */
+async function endSession() {
+  updateStatus("Ending Call");
+  console.log(`Ending session: ${internal_call_id}`);
+
+  // hangup the webrtc connection
+  bandwidthRtc.disconnect();
+
+  // clear out any remaining connections
+  other_callers.forEach(function (caller) {
+    removeCaller(caller);
+  });
+
+  try {
+    var res = await fetch("/endSession?identifier=" + internal_call_id);
+
+    // basic error handling
+    if (res.status !== 200) {
+      console.log(res);
+      alert("Failed to end your session: " + res.status);
+      return;
+    } else {
+      updateStatus("Call Ended");
+    }
+  } catch (error) {
+    console.log(`failed to end the session ${error}`);
+    console.log("we'll keep cleaning up though");
+  }
+}
+
+/**
+ * Place a call to BAND to start on the PSTN, leverages serverside function
+ * @param {string} number a +1 NANP number
+ * @param identifier - something to identify this room/call
+ */
+async function placePSTNCall(number, identifier) {
+  // then start the call
+  updateStatus("dialing");
+  var res = await fetch(
+    "/startPSTNCall?to_num=" + number + "&identifier=" + identifier
+  );
+  const json = await res.json();
+  return json;
+  //   console.log(json);
+}
+
+/**
+ * Setup our listeners for the events from the media server
+ */
+window.addEventListener("load", (event) => {
+  bandwidthRtc.onStreamAvailable((rtcStream) => {
+    connectStream(rtcStream);
+  });
+
+  bandwidthRtc.onStreamUnavailable((endpointId) => {
+    disconnectEndpoint(endpointId);
+  });
+});
+
+function connectStream(rtcStream) {
+  // check if this is already connected
+  if (other_callers.indexOf(rtcStream.endpointId) > -1) {
+    console.log(`duplicate receipt of:${rtcStream.endpointId}`);
+    return;
+  }
+  console.log(`receiving media! ${rtcStream.endpointId}`);
+  console.log(rtcStream);
+
+  // get the sound flowing
+  var elType = "audio";
+  if (videoEnabled) {
+    elType = "video";
+  }
+  var mediaEl = document.createElement(elType);
+  mediaEl.id = rtcStream.endpointId;
+  mediaEl.autoplay = true;
+  mediaEl.srcObject = rtcStream.mediaStream;
+
+  // keep track of the streams we're connected to
+  other_callers.push(rtcStream.endpointId);
+
+  // clean up any ringing audio
+  stopAudio("ring");
+  // update status
+  updateStatus("In Call");
+
+  // either append it to mediaDiv, or give it back to the calling script/file
+  if (typeof onNewStream === "function") {
+    console.log("calling customer onNewStream function");
+    onNewStream(mediaEl, rtcStream);
+  } else {
+    console.log("no onNewStream functin defined");
+    if (mediaDiv) {
+      document.getElementById(mediaDiv).appendChild(mediaEl);
+    } else {
+      document.body.appendChild(mediaEl);
+    }
+  }
+}
+function disconnectEndpoint(endpointId) {
+  console.log("no longer receiving media for endpoint: " + endpointId);
+
+  // if this endpoint is still active on the call
+  if (other_callers.indexOf(endpointId) > -1) {
+    removeCaller(endpointId);
+
+    // if there is no one left in the call
+    if (other_callers.length == 0) {
+      updateStatus("Call Ended");
+      console.log(`All callers are off the line, ending call`);
+      // alert("The call is over");
+      // optional function to call when there are no other participants left
+      if (typeof allCallsEnded != "undefined") {
+        allCallsEnded();
+      }
+    }
+
+    if (typeof onEndStream === "function") {
+      console.log("calling customer onNewStream function");
+      onEndStream(endpointId);
+    }
+  } else {
+    console.log(
+      `We got a disconnect on ${endpointId} but have no media for, this is common as a second notice of an ended stream`
+    );
+  }
+}
+/**
+ * remove the div for this caller
+ * @param {*} id
+ */
+function removeCaller(id) {
+  let index = other_callers.indexOf(id);
+  if (index > -1) {
+    other_callers.splice(index, 1);
+    audioElement = document.getElementById(id);
+    audioElement.srcObject = undefined;
+    audioElement.remove();
+  }
+}
+
+/**
+ * Show a "vanity" view of your camera as it will be displayed to
+ *  other participants
+ * @param {string} cam_device the device id for the device to stream from (list available via listCameras)
+ * @param {string} video_id the dom element id that we should make this video a child of
+ * @param {json} video_constraints Any additional video constraints you'd like to add to this video
+ */
+async function show_vanity_mirror(
+  cam_device,
+  video_id,
+  video_constraints = {}
+) {
+  // disable any current device, this is important to turn off the cam (and it's led light)
+  if (local_video_stream) {
+    var tracks = local_video_stream.getTracks();
+    tracks.forEach(function (track) {
+      track.stop();
+    });
+  }
+
+  // allows for an option like "disable" in the cam selector
+  if (cam_device == "none") {
+    video_constraints = false;
+    document.getElementById(video_id).srcObject = null;
+    return;
+  } else {
+    video_constraints.deviceId = { exact: cam_device };
+  }
+
+  try {
+    local_video_stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: video_constraints,
+    });
+    document.getElementById(video_id).srcObject = local_video_stream;
+  } catch (error) {
+    console.log(`Failed to acquire local video: ${error.message}`);
+    alert("Sorry, we can't proceed without access to your camera");
+  }
+}
+
+//
+// List and select devices
+async function listCameras(selectId) {
+  return getDeviceList("videoinput");
+}
+async function listMicrophones(selectId) {
+  return getDeviceList("audioinput");
+}
+
+async function getDeviceList(type) {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  let deviceList = [];
+
+  devices.forEach(function (device) {
+    if (device.kind == type) {
+      deviceList.push({ label: device.label, deviceId: device.deviceId });
+    }
+  });
+
+  return deviceList;
+}
+
+//
+// Manipulate the media streams
+/**
+ * Mutes the mic, note that this assumes one mic stream
+ */
+var is_mic_enabled = true;
+function muteFlip() {
+  is_mic_enabled = !is_mic_enabled;
+  if (is_mic_enabled) {
+    set_mute_all("audio", false);
+  } else {
+    set_mute_all("audio", true);
+  }
+}
+function mute() {
+  // if already muted
+  if (is_mic_enabled) {
+    is_mic_enabled = false;
+    set_mute_all("audio", true);
+  }
+}
+function unmute() {
+  // if already unmuted
+  if (!is_mic_enabled) {
+    is_mic_enabled = true;
+    set_mute_all("audio", false);
+  }
+}
+/**
+ * 'Mutes' the camera
+ */
+var is_cam_enabled = true;
+function video_muteFlip() {
+  is_cam_enabled = !is_cam_enabled;
+  if (is_cam_enabled) {
+    set_mute_all("video", false);
+  } else {
+    set_mute_all("video", true);
+  }
+}
+function video_mute() {
+  // if already muted
+  if (is_cam_enabled) {
+    is_cam_enabled = false;
+    set_mute_all("video", true);
+  }
+}
+function video_unmute() {
+  if (!is_cam_enabled) {
+    is_cam_enabled = true;
+    set_mute_all("video", false);
+  }
+}
+
+/**
+ * Set the mute state for all video or audio tracks
+ * @param {string} type 'audio' or 'video'
+ * @param {boolean} mute_state true for mute, false for unmute
+ */
+function set_mute_all(type, mute_state) {
+  my_media_stream.getTracks().forEach(function (stream) {
+    // console.log("looping");
+    // console.log(stream);
+    if (stream.kind == type) {
+      stream.enabled = !mute_state;
+    }
+  });
+}
+
+//
+// PlayAudio File in Browser
+//
+const pre_sound = "play_sound_id_";
+function playAudio(name, url) {
+  var sound_el = document.createElement("audio");
+  // sound.id = ;
+  sound_el.autoplay = true;
+  sound_el.id = pre_sound + name;
+  sound_el.loop = true;
+  sound_el.src = url;
+  if (mediaDiv) {
+    document.getElementById(mediaDiv).appendChild(sound_el);
+  } else {
+    document.body.appendChild(sosound_elund);
+  }
+}
+function stopAudio(name) {
+  sound_el = document.getElementById(pre_sound + name);
+  if (sound_el) {
+    // make sure the sound stops right away
+    sound_el.muted = true;
+    sound_el.remove();
+  }
+}
+
+//
+// LOGGING / STATUS UPDATES
+//
+function setStatusDiv(status_div_id) {
+  if (!statusDiv) {
+    statusDiv = status_div_id;
+  }
+}
+/**
+ * set a the innerHTML of a div to the status,
+ *  expects statusDiv to be set
+ * @param {*} status
+ */
+function updateStatus(status) {
+  if (!statusDiv) {
+    console.log(
+      `WARNING: statusDiv was never set (via setStatusDiv()), would have set ${status}`
+    );
+  } else {
+    document.getElementById(statusDiv).innerHTML = status;
+  }
+}
+
+//
+// This gets the debug info into a div on the screen
+// helpful for debugging mobile clients
+if (DEBUG) {
+  if (typeof console != "undefined") {
+    if (typeof console.log != "undefined") {
+      console.orig_log = console.log;
+    } else console.orig_log = function () {};
+  }
+
+  console.log = function (message) {
+    console.orig_log(message);
+    if (logDiv) {
+      document.getElementById(logDiv).append(">" + message);
+      document.getElementById(logDiv).append(document.createElement("br"));
+    }
+  };
+  console.error = console.debug = console.info = console.log;
+}
